405e468d8762d87320438c9d61eac928c7708ed4
Vlatko Spoljaric <vlatko.spoljaric@coinme.com>
2023-05-30 20:34:23 +0200

    Added module internal-user-service

---
+++ internal-user-service/src/main/java/agency/community/InternalUserServiceApplication.java
@@ -0,0 +1,12 @@
+package agency.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternalUserServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InternalUserServiceApplication.class, args);
+  }
+}
